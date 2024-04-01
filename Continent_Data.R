f44cab8334ec60fbe82cc4441d7dd2362c82189d
Schrodinger73 <63200376+Schrodinger73@users.noreply.github.com>
2024-04-01 17:52:12 +0530

    Update Continent_Data.R

--- Continent_Data.R
+++ Continent_Data.R
@@ -3,4 +3,4 @@ suicide <- read.csv("suicide.csv")
 v <- country_info(countries = suicide$country, fields = "continents")
 continent <- v$continents
 suicide$continent <- continent
-write.csv(suicide, file = "suicide.csv")
+write.csv(suicide, file = "suicide_cleaned.csv")
